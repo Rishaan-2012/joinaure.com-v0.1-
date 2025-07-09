df07f168840f0e9c1536ceb025d0a30a083930df
Rishaan-2012 <rishaan804@gmail.com>
2025-07-09 16:17:32 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,3 @@
-import Head from "next/head"
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <Head>
-        <title>Aure – Modern Money Management</title>
-      </Head>
-      <body>{children}</body>
-    </html>
-  )
-}
-
 import type { Metadata } from 'next'
 import './globals.css'
 
