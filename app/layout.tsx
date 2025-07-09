d942cab18908d6be0a95bc052550df1019c8ccf8
Rishaan-2012 <rishaan804@gmail.com>
2025-07-09 16:31:48 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Aure - Modern Money Management',
+  description: 'Join Aure Today',
   generator: 'v0.dev',
 }
 
